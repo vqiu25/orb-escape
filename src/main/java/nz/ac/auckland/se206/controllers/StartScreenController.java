a3a17c5290ca682e9c28be210eb5a199c55de130
wcho912 <wcho912@aucklanduni.ac.nz>
2023-09-15 23:01:00 +1200

    Refactor Code: Removed commented out code that is no longer being used.

--- src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
@@ -8,22 +8,6 @@ public class StartScreenController extends ControllerMethods {
   /** Switches the scene from the start GUI to tutorial GUI. */
   @FXML
   private void onLaunchGame() {
-
-    // Syntax for new notification library: look through this to see possible methods
-    // https://github.com/HanSolo/Enzo/blob/master/src/main/java/eu.hansolo.enzo/notification/Notification.java
-    // Notification info =
-    //     new Notification(
-    //         "title",
-    //         "info-message, this is me testing how long the text can be before it screws up
-    // bruh");
-
-    // Notifier.setPopupLocation(App.getStage(), Pos.TOP_CENTER);
-    // Notifier.setOffsetY(50);
-    // Notifier.setWidth(400);
-    // Notifier.setHeight(100);
-
-    // Notifier.INSTANCE.notify(info);
-
     // Change scene to show back story:
     App.setScene(AppScene.STORY);
   }
