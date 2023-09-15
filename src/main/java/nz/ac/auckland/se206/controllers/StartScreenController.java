0481540f20325d315b55e2bb1510e98f5991ae64
victor <vqiu25@gmail.com>
2023-09-16 01:03:34 +1200

    Feature: Updated start screen to match resolution and theme

--- src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
@@ -1,20 +1,77 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppScene;
 
 public class StartScreenController extends ControllerMethods {
-  /** Switches the scene from the start GUI to tutorial GUI. */
+  @FXML private ImageView startOneButton;
+  @FXML private ImageView startTwoButton;
+  @FXML private ImageView startThreeButton;
+  @FXML private ImageView optionsOneButton;
+  @FXML private ImageView optionsTwoButton;
+  @FXML private ImageView optionsThreeButton;
+  @FXML private ImageView quitOneButton;
+  @FXML private ImageView quitTwoButton;
+  @FXML private ImageView quitThreeButton;
+
   @FXML
-  private void onLaunchGame() {
-    // Change scene to show back story:
+  private void startHover(MouseEvent event) {
+    startTwoButton.setOpacity(1);
+  }
+
+  @FXML
+  private void startUnhover(MouseEvent event) {
+    startTwoButton.setOpacity(0);
+  }
+
+  @FXML
+  private void startPressed(MouseEvent event) {
+    startThreeButton.setOpacity(1);
+  }
+
+  @FXML
+  private void startReleased(MouseEvent event) {
     App.setScene(AppScene.STORY);
   }
 
-  /** Quits the game. */
   @FXML
-  private void onLeaveGame() {
+  private void optionsHover(MouseEvent event) {
+    optionsTwoButton.setOpacity(1);
+  }
+
+  @FXML
+  private void optionsUnhover(MouseEvent event) {
+    optionsTwoButton.setOpacity(0);
+  }
+
+  @FXML
+  private void optionsPressed(MouseEvent event) {
+    optionsThreeButton.setOpacity(1);
+  }
+
+  @FXML
+  private void optionsReleased(MouseEvent event) {}
+
+  @FXML
+  private void quitHover(MouseEvent event) {
+    quitTwoButton.setOpacity(1);
+  }
+
+  @FXML
+  private void quitUnhover(MouseEvent event) {
+    quitTwoButton.setOpacity(0);
+  }
+
+  @FXML
+  private void quitPressed(MouseEvent event) {
+    quitThreeButton.setOpacity(1);
+  }
+
+  @FXML
+  private void quitReleased(MouseEvent event) {
     System.exit(0);
   }
 }
