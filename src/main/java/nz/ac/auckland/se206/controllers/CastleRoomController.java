528f9a33cea7cf56fd24244dcdfa7d9e76c383f9
wcho912 <wcho912@aucklanduni.ac.nz>
2023-09-16 23:45:08 +1200

    Feature: Added increment and decrement buttons and animations for padlock.

--- src/main/java/nz/ac/auckland/se206/controllers/CastleRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CastleRoomController.java
@@ -22,6 +22,33 @@ public class CastleRoomController {
   @FXML private ImageView chestOpenedOrbOutline;
   @FXML private ImageView chestEmpty;
 
+  // Lock 1:
+  @FXML private ImageView lockOneIncrement;
+  @FXML private ImageView lockOneIncrementHovered;
+  @FXML private ImageView lockOneIncrementPressed;
+
+  @FXML private ImageView lockOneDecrement;
+  @FXML private ImageView lockOneDecrementHovered;
+  @FXML private ImageView lockOneDecrementPressed;
+
+  // Lock 2:
+  @FXML private ImageView lockTwoIncrement;
+  @FXML private ImageView lockTwoIncrementHovered;
+  @FXML private ImageView lockTwoIncrementPressed;
+
+  @FXML private ImageView lockTwoDecrement;
+  @FXML private ImageView lockTwoDecrementHovered;
+  @FXML private ImageView lockTwoDecrementPressed;
+
+  // Lock 3:
+  @FXML private ImageView lockThreeIncrement;
+  @FXML private ImageView lockThreeIncrementHovered;
+  @FXML private ImageView lockThreeIncrementPressed;
+
+  @FXML private ImageView lockThreeDecrement;
+  @FXML private ImageView lockThreeDecrementHovered;
+  @FXML private ImageView lockThreeDecrementPressed;
+
   // Methods for back button animations:
   @FXML
   private void backHover(MouseEvent event) {
@@ -73,6 +100,151 @@ public class CastleRoomController {
     // TODO: check if the answer is correct
   }
 
-  // Methods for chest animations:
+  // Methods for pad lock animations:
+  // Lock 1:
+  @FXML
+  private void lockOneIncrementHover(MouseEvent event) {
+    lockOneIncrementHovered.setOpacity(1);
+  }
 
+  @FXML
+  private void lockOneIncrementUnhover(MouseEvent event) {
+    lockOneIncrementHovered.setOpacity(0);
+  }
+
+  @FXML
+  private void lockOneIncrementPressed(MouseEvent event) {
+    lockOneIncrementPressed.setOpacity(1);
+  }
+
+  @FXML
+  private void lockOneIncrementReleased(MouseEvent event) {
+    lockOneIncrementPressed.setOpacity(0);
+
+    System.out.println("Lock one increment pressed");
+
+    // TODO: update label
+  }
+
+  @FXML
+  private void lockOneDecrementHover(MouseEvent event) {
+    lockOneDecrementHovered.setOpacity(1);
+  }
+
+  @FXML
+  private void lockOneDecrementUnhover(MouseEvent event) {
+    lockOneDecrementHovered.setOpacity(0);
+  }
+
+  @FXML
+  private void lockOneDecrementPressed(MouseEvent event) {
+    lockOneDecrementPressed.setOpacity(1);
+  }
+
+  @FXML
+  private void lockOneDecrementReleased(MouseEvent event) {
+    lockOneDecrementPressed.setOpacity(0);
+
+    System.out.println("Lock one decrement pressed");
+
+    // TODO update label
+  }
+
+  // Lock 2:
+  @FXML
+  private void lockTwoIncrementHover(MouseEvent event) {
+    lockTwoIncrementHovered.setOpacity(1);
+  }
+
+  @FXML
+  private void lockTwoIncrementUnhover(MouseEvent event) {
+    lockTwoIncrementHovered.setOpacity(0);
+  }
+
+  @FXML
+  private void lockTwoIncrementPressed(MouseEvent event) {
+    lockTwoIncrementPressed.setOpacity(1);
+  }
+
+  @FXML
+  private void lockTwoIncrementReleased(MouseEvent event) {
+    lockTwoIncrementPressed.setOpacity(0);
+
+    System.out.println("Lock two increment pressed");
+
+    // TODO update label
+  }
+
+  @FXML
+  private void lockTwoDecrementHover(MouseEvent event) {
+    lockTwoDecrementHovered.setOpacity(1);
+  }
+
+  @FXML
+  private void lockTwoDecrementUnhover(MouseEvent event) {
+    lockTwoDecrementHovered.setOpacity(0);
+  }
+
+  @FXML
+  private void lockTwoDecrementPressed(MouseEvent event) {
+    lockTwoDecrementPressed.setOpacity(1);
+  }
+
+  @FXML
+  private void lockTwoDecrementReleased(MouseEvent event) {
+    lockTwoDecrementPressed.setOpacity(0);
+
+    System.out.println("Lock two decrement pressed");
+
+    // TODO update label
+  }
+
+  // Lock 3:
+  @FXML
+  private void lockThreeIncrementHover(MouseEvent event) {
+    lockThreeIncrementHovered.setOpacity(1);
+  }
+
+  @FXML
+  private void lockThreeIncrementUnhover(MouseEvent event) {
+    lockThreeIncrementHovered.setOpacity(0);
+  }
+
+  @FXML
+  private void lockThreeIncrementPressed(MouseEvent event) {
+    lockThreeIncrementPressed.setOpacity(1);
+  }
+
+  @FXML
+  private void lockThreeIncrementReleased(MouseEvent event) {
+    lockThreeIncrementPressed.setOpacity(0);
+
+    System.out.println("Lock three increment pressed");
+
+    // TODO update label
+  }
+
+  @FXML
+  private void lockThreeDecrementHover(MouseEvent event) {
+    lockThreeDecrementHovered.setOpacity(1);
+  }
+
+  @FXML
+  private void lockThreeDecrementUnhover(MouseEvent event) {
+    lockThreeDecrementHovered.setOpacity(0);
+  }
+
+  @FXML
+  private void lockThreeDecrementPressed(MouseEvent event) {
+    lockThreeDecrementPressed.setOpacity(1);
+  }
+
+  @FXML
+  private void lockThreeDecrementReleased(MouseEvent event) {
+    lockThreeDecrementPressed.setOpacity(0);
+
+    System.out.println("Lock three decrement pressed");
+
+    // TODO update label
+  }
 }
