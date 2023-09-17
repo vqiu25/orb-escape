c62f829ad5348f2a66865f1406d8751ead03ff9e
wcho912 <wcho912@aucklanduni.ac.nz>
2023-09-18 00:29:52 +1200

    Bugfix: Set roomOrbCollected boolean to false to ensure that the terminal can only be accessed once all orbs have been found.

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -27,7 +27,7 @@ public class GameState {
   // Indicates if orbs have been collected:
   public static boolean isForrestOrbCollected = false;
   public static boolean isCastleOrbCollected = false;
-  public static boolean isRoomOrbCollected = true; // TODO: add this to room!
+  public static boolean isRoomOrbCollected = false; // TODO: add this to room!
 
   // Indicates if all the orbs have been placed in the terminal
   public static boolean isOrbsPlaced = false;
